77f10a2c08e79f0cad37927e4c17ec8f0b44afd4
Carlndigo <spurs52027877@gmail.com>
2026-05-11 04:39:37 +0100

    Add files via upload

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -2,6 +2,7 @@
     "bao20253d": {
         "author": "Bao, Yanqi and Ding, Tianyu and Huo, Jing and Liu, Yaoli and Li, Yuxin and Li, Wenbin and Gao, Yang and Luo, Jiebo",
         "journal": "IEEE Transactions on Circuits and Systems for Video Technology",
+        "keywords": "type:Survey, other:Background",
         "number": "7",
         "pages": "6832--6852",
         "publisher": "IEEE",
@@ -13,6 +14,7 @@
     "han2026websplatter": {
         "author": "Han, Yudong and Xu, Chao and Ye, Xiaodan and Bi, Weichen and Dong, Zilong and Ma, Yun",
         "journal": "arXiv preprint arXiv:2602.03207",
+        "keywords": "type:Hybrid_Methods, other:Performance",
         "title": "WebSplatter: Enabling Cross-Device Efficient Gaussian Splatting in Web Browsers via WebGPU",
         "type": "article",
         "year": "2026"
@@ -20,6 +22,7 @@
     "kocabas2024hugs": {
         "author": "Kocabas, Muhammed and Chang, Jen-Hao Rick and Gabriel, James and Tuzel, Oncel and Ranjan, Anurag",
         "booktitle": "Proceedings of the IEEE/CVF conference on computer vision and pattern recognition",
+        "keywords": "type:Animatable_Avatars, other:Rigging",
         "pages": "505--515",
         "title": "Hugs: Human gaussian splats",
         "type": "inproceedings",
@@ -28,6 +31,7 @@
     "mir2025aha": {
         "author": "Mir, Aymen and Wang, Jian and Guler, Riza Alp and Guo, Chuan and Pons-Moll, Gerard and Zhou, Bing",
         "journal": "arXiv preprint arXiv:2511.09827",
+        "keywords": "type:Animatable_Avatars, other:Scene_Integration",
         "title": "AHA! Animating Human Avatars in Diverse Scenes with Gaussian Splatting",
         "type": "article",
         "year": "2025"
@@ -35,6 +39,7 @@
     "qian20243dgs": {
         "author": "Qian, Zhiyin and Wang, Shaofei and Mihajlovic, Marko and Geiger, Andreas and Tang, Siyu",
         "booktitle": "Proceedings of the IEEE/CVF conference on computer vision and pattern recognition",
+        "keywords": "type:Animatable_Avatars, other:Deformation",
         "pages": "5020--5030",
         "title": "3dgs-avatar: Animatable avatars via deformable 3d gaussian splatting",
         "type": "inproceedings",
@@ -43,6 +48,7 @@
     "qian2024gaussianavatars": {
         "author": "Qian, Shenhan and Kirschstein, Tobias and Schoneveld, Liam and Davoli, Davide and Giebenhain, Simon and Nie{\\ss}ner, Matthias",
         "booktitle": "Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition",
+        "keywords": "type:Animatable_Avatars, other:Rigging",
         "pages": "20299--20309",
         "title": "Gaussianavatars: Photorealistic head avatars with rigged 3d gaussians",
         "type": "inproceedings",
@@ -51,6 +57,7 @@
     "song2026vrgaussianavatar": {
         "author": "Song, Hail and Yoon, Boram and Yang, Seokhwan and Kang, Seoyoung and Kim, Hyunjeong and Metzmacher, Henning and Woo, Woontack",
         "journal": "arXiv preprint arXiv:2602.01674",
+        "keywords": "type:User_Study, other:Perception",
         "title": "VRGaussianAvatar: Integrating 3D Gaussian Avatars into VR",
         "type": "article",
         "year": "2026"
@@ -58,6 +65,7 @@
     "wang20253d": {
         "author": "Wang, Yating and Wang, Xuan and Yi, Ran and Fan, Yanbo and Hu, Jichen and Zhu, Jingcheng and Ma, Lizhuang",
         "booktitle": "Proceedings of the Computer Vision and Pattern Recognition Conference",
+        "keywords": "type:Animatable_Avatars, other:Dynamic_Appearance",
         "pages": "21117--21126",
         "title": "3d gaussian head avatars with expressive dynamic appearances by compact tensorial representations",
         "type": "inproceedings",
@@ -65,12 +73,16 @@
     },
     "wijayathungaenhancing": {
         "author": "WIJAYATHUNGA, WMRDB",
+        "journal": "arXiv preprint arXiv:2507.07000",
+        "keywords": "type:Hybrid_Methods, other:Mesh",
         "title": "Enhancing non-Rigid 3D Model Deformations Using Mesh-based Gaussian Splatting",
-        "type": "article"
+        "type": "article",
+        "year": "2025"
     },
     "yao2025riggs": {
         "author": "Yao, Yuxin and Deng, Zhi and Hou, Junhui",
         "booktitle": "Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition",
+        "keywords": "type:Animatable_Avatars, other:Rigging",
         "pages": "5592--5601",
         "title": "Riggs: Rigging of 3d gaussians for modeling articulated objects in videos",
         "type": "inproceedings",
