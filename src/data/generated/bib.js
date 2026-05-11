3f2b8970ffabf02e9bd6e96ffac2bf782177509e
Carlndigo <spurs52027877@gmail.com>
2026-05-11 04:32:39 +0100

    Add files via upload

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,79 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
+    "bao20253d": {
+        "author": "Bao, Yanqi and Ding, Tianyu and Huo, Jing and Liu, Yaoli and Li, Yuxin and Li, Wenbin and Gao, Yang and Luo, Jiebo",
+        "journal": "IEEE Transactions on Circuits and Systems for Video Technology",
+        "number": "7",
+        "pages": "6832--6852",
         "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+        "title": "3d gaussian splatting: Survey, technologies, challenges, and opportunities",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "volume": "35",
+        "year": "2025"
+    },
+    "han2026websplatter": {
+        "author": "Han, Yudong and Xu, Chao and Ye, Xiaodan and Bi, Weichen and Dong, Zilong and Ma, Yun",
+        "journal": "arXiv preprint arXiv:2602.03207",
+        "title": "WebSplatter: Enabling Cross-Device Efficient Gaussian Splatting in Web Browsers via WebGPU",
+        "type": "article",
+        "year": "2026"
+    },
+    "kocabas2024hugs": {
+        "author": "Kocabas, Muhammed and Chang, Jen-Hao Rick and Gabriel, James and Tuzel, Oncel and Ranjan, Anurag",
+        "booktitle": "Proceedings of the IEEE/CVF conference on computer vision and pattern recognition",
+        "pages": "505--515",
+        "title": "Hugs: Human gaussian splats",
+        "type": "inproceedings",
+        "year": "2024"
+    },
+    "mir2025aha": {
+        "author": "Mir, Aymen and Wang, Jian and Guler, Riza Alp and Guo, Chuan and Pons-Moll, Gerard and Zhou, Bing",
+        "journal": "arXiv preprint arXiv:2511.09827",
+        "title": "AHA! Animating Human Avatars in Diverse Scenes with Gaussian Splatting",
+        "type": "article",
+        "year": "2025"
+    },
+    "qian20243dgs": {
+        "author": "Qian, Zhiyin and Wang, Shaofei and Mihajlovic, Marko and Geiger, Andreas and Tang, Siyu",
+        "booktitle": "Proceedings of the IEEE/CVF conference on computer vision and pattern recognition",
+        "pages": "5020--5030",
+        "title": "3dgs-avatar: Animatable avatars via deformable 3d gaussian splatting",
+        "type": "inproceedings",
+        "year": "2024"
+    },
+    "qian2024gaussianavatars": {
+        "author": "Qian, Shenhan and Kirschstein, Tobias and Schoneveld, Liam and Davoli, Davide and Giebenhain, Simon and Nie{\\ss}ner, Matthias",
+        "booktitle": "Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition",
+        "pages": "20299--20309",
+        "title": "Gaussianavatars: Photorealistic head avatars with rigged 3d gaussians",
+        "type": "inproceedings",
+        "year": "2024"
+    },
+    "song2026vrgaussianavatar": {
+        "author": "Song, Hail and Yoon, Boram and Yang, Seokhwan and Kang, Seoyoung and Kim, Hyunjeong and Metzmacher, Henning and Woo, Woontack",
+        "journal": "arXiv preprint arXiv:2602.01674",
+        "title": "VRGaussianAvatar: Integrating 3D Gaussian Avatars into VR",
+        "type": "article",
+        "year": "2026"
+    },
+    "wang20253d": {
+        "author": "Wang, Yating and Wang, Xuan and Yi, Ran and Fan, Yanbo and Hu, Jichen and Zhu, Jingcheng and Ma, Lizhuang",
+        "booktitle": "Proceedings of the Computer Vision and Pattern Recognition Conference",
+        "pages": "21117--21126",
+        "title": "3d gaussian head avatars with expressive dynamic appearances by compact tensorial representations",
+        "type": "inproceedings",
+        "year": "2025"
+    },
+    "wijayathungaenhancing": {
+        "author": "WIJAYATHUNGA, WMRDB",
+        "title": "Enhancing non-Rigid 3D Model Deformations Using Mesh-based Gaussian Splatting",
+        "type": "article"
+    },
+    "yao2025riggs": {
+        "author": "Yao, Yuxin and Deng, Zhi and Hou, Junhui",
+        "booktitle": "Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition",
+        "pages": "5592--5601",
+        "title": "Riggs: Rigging of 3d gaussians for modeling articulated objects in videos",
+        "type": "inproceedings",
+        "year": "2025"
     }
 };
